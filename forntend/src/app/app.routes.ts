7024875903ced9486127c59d3b1b155af6508f16
joseamerico-git <doichejunior@gmail.com>
2024-02-17 23:42:24 -0300

    commit008

--- forntend/src/app/app.routes.ts
+++ forntend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'login',pathMatch:'full'},
+    {path:'login',component:LoginComponent},
+    {path:'sigIn',component:SignInComponent},
+    {path:'**',redirectTo:'login',pathMatch:'full'}
+];
