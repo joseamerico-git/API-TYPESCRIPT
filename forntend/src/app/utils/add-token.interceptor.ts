8f08771cbd372b9bda460b159616125f33b23813
joseamerico-git <doichejunior@gmail.com>
2024-02-22 01:45:35 -0300

    commit0015

--- forntend/src/app/utils/add-token.interceptor.ts
+++ forntend/src/app/utils/add-token.interceptor.ts
@@ -1,5 +1,11 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const addTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('token');
+   // const headers = new HttpHeaders().set('authorization',`Bearer ${token}`)
+   // return this.http.get<Product[]>(`${this.myAppUrl}${this.myApiUrl}`,{headers:headers})
+   if(token){
+    req = req.clone({setHeaders:{Autorization: `Bearer ${token}`}})
+   }
   return next(req);
 };
